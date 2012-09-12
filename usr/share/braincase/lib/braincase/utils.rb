a63395e012e8f8d4c8f98e1eeb2d20d4fd56caff
Robert McLeod <hamstar@telescum.co.nz>
2012-09-12 18:37:27 +1200

    added the send email function to braincase utils

--- usr/share/braincase/lib/braincase/utils.rb
+++ usr/share/braincase/lib/braincase/utils.rb
@@ -1,8 +1,10 @@
 require 'yaml'
+require 'net/smtp'
 
 module Braincase
 
   CONFIG_FILE = "/etc/braincase/config"
+  HOSTNAME = `hostname -f`.chomp
 
   def Braincase.config(section='')
     
@@ -19,5 +21,26 @@ module Braincase
 
   def Braincase.is_root?
     `echo $USER`.chomp == "root"
+  end
+
+  # allows us to send emails
+  def Braincase.send_email(to,opts={})
+    opts[:server]      ||= "localhost"
+    opts[:from]        ||= "braincase@#{Braincase::HOSTNAME}"
+    opts[:from_alias]  ||= "Braincase"
+    opts[:subject]     ||= "Message from Braincase"
+    opts[:body]        ||= "Nothing to report!"
+
+    msg = <<END_OF_MESSAGE
+From: #{opts[:from_alias]} <#{opts[:from]}>
+To: <#{to}>
+Subject: #{opts[:subject]}
+
+#{opts[:body]}
+END_OF_MESSAGE
+
+    Net::SMTP.start(opts[:server]) do |smtp|
+      smtp.send_message msg, opts[:from], to
+    end
   end
 end
