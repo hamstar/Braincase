d894f1bd23e67a6fecca2134d3286c9c3715acd2
Robert <hamstar@telescum.co.nz>
2012-10-31 15:18:57 +1300

    refs #292: fix typo

--- usr/share/braincase/lib/braincase/user_creator.rb
+++ usr/share/braincase/lib/braincase/user_creator.rb
@@ -49,7 +49,7 @@ module Braincase
 
     def apply_permissions
       FileUtils.chown_R nil, "www-data", @user.dirs[:doku]
-      FileUtils.chmod_R 0700, @user.dirs[:doku]
+      FileUtils.chmod_R 0770, @user.dirs[:doku]
       FileUtils.chown_R nil, "www-data", @user.dirs[:logs]
       FileUtils.chmod_R "g+w", @user.dirs[:logs]
       FileUtils.chmod "g+w", @user.logs[:restore]
